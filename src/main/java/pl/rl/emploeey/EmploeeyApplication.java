22b24a4c06747794da476d3eb786cd5f488542e7
Robert Lewicki <lewicki.robert@onet.pl>
2021-05-30 19:02:40 +0200

    Initial commit

---
+++ src/main/java/pl/rl/emploeey/EmploeeyApplication.java
@@ -0,0 +1,13 @@
+package pl.rl.emploeey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploeeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmploeeyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/rl/emploeey/EmploeeyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rl.emploeey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmploeeyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
